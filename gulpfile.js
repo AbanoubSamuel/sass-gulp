55426c5ae1570b573d8b97db2fe4509a75a7f2b1
AbanoubSamuel <abanoub.samuel@hotmail.com>
2022-09-30 22:30:38 +0200

    fixed dest structure

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
     return src('./src/**/*.scss')
         .pipe(sass())
-        .pipe(dest('./css/'))
+        .pipe(dest('./dest/'))
 }
 
 function watchTask() {
